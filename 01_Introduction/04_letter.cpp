08da5bc77289fe96b84ca15e783bc20213cd10cc
benedictusdps <benedictusdps@gmail.com>
2022-03-23 20:26:54 +0700

    Add files from upload

---
+++ 01_Introduction/04_letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "I will get a Software Engineering or\n";
+  std::cout << "a Data Scientist job.\n";
+  std::cout << "Benedictus, 23/03/2022\n";
+  
+  
+}
