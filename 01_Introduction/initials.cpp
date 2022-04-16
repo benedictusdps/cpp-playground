8cc027cc762dc453add988d807914c51457349e6
benedictusdps <benedictusdps@gmail.com>
2022-04-16 20:53:55 +0700

    Add files from upload

---
+++ 01_Introduction/initials.cpp
@@ -0,0 +1,14 @@
+// I like eating chicken wings.
+
+#include <iostream>
+
+int main() {
+  // Your program here:
+  std::cout << "BBBB   SSS \n";
+  std::cout << "B   B S   S\n";
+  std::cout << "B   B S\n";
+  std::cout << "BBBB   SSS \n";
+  std::cout << "B   B     S\n";
+  std::cout << "B   B S   S\n";
+  std::cout << "BBBB   SSS \n";
+}
